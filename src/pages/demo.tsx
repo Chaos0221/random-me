4196682049d11f15daadd7fba2ca5a243b8ed0ae
zeng <kilig0221@gmail.com>
2023-03-05 18:27:34 +0800

    init: neo me

---
+++ src/pages/demo.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/demo.module.css'
+
+export default function Demo() {
+    return (
+        <section className={styles.demo}>
+            Neo World, Neo Me.
+        </section>
+    )
+}
